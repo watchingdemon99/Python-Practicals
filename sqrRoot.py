6e7c99f3ebba815a0f5fd94c6cd8cbbcd4469fc1
watchingdemon99 <92046369+watchingdemon99@users.noreply.github.com>
2022-04-11 08:19:39 +0530

    Add files via upload

---
+++ sqrRoot.py
@@ -0,0 +1,4 @@
+# Program to find Square Root
+a = int(input("Enter a number "))
+b = (a**0.5)
+print("The square root of the number is ",b )
---
+++ swap.py
@@ -0,0 +1,9 @@
+# Program to swap two numbers
+a = int(input("Enter the value of a "))
+b = int(input("Enter the value of b "))
+a += b
+b = a-b
+a -= b
+
+print(f"The value of a is {a} and value of b is {b}")
+
