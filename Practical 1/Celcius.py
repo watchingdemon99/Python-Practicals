4b944bf86ada53355e001a9378af24d9137a1d46
watchingdemon99 <92046369+watchingdemon99@users.noreply.github.com>
2022-07-12 21:37:25 +0530

    Add files via upload

---
+++ Practical 1/Celcius.py
@@ -0,0 +1,5 @@
+# Program to convert Celcius to Farenheit
+
+a = int(input("Enter the temperature "))
+b = a *(1.8)+32
+print(f"The temperature in farenheit is {b}")
---
+++ Practical 1/aot.py
@@ -0,0 +1,5 @@
+# Area of Triangle
+b = int(input("Enter the base "))
+h = int(input("Enter the height "))
+
+print(f"The area of the triangle is {0.5 * b * h} ")
