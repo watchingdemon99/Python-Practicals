ddcbaa199a0cdf59e5297a11a2d66b420fe70903
watchingdemon99 <92046369+watchingdemon99@users.noreply.github.com>
2022-07-12 21:39:45 +0530

    Delete swap.py

--- swap.py
+++
@@ -1,9 +0,0 @@
-# Program to swap two numbers
-a = int(input("Enter the value of a "))
-b = int(input("Enter the value of b "))
-a += b
-b = a-b
-a -= b
-
-print(f"The value of a is {a} and value of b is {b}")
-
